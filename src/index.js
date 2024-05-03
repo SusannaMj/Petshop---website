e67d41df86ae8e3b761ebb6af1f19658d66a9258
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-05-04 00:00:34 +1000

    added like button click function
    - like/dislike

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ function showDropdown() {
 function hideDropdown() {
   document.getElementById("dropdown-content").classList.remove("show");
 }
+
+function like(x) {
+  if (x.classList.contains("fa-regular")) {
+    x.classList.remove("fa-regular");
+    x.classList.add("fa-solid");
+  } else {
+    x.classList.remove("fa-solid");
+    x.classList.add("fa-regular");
+  }
+}
